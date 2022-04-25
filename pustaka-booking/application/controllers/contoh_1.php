2ce7e93f8c06474a5c22d0c96b7627ae9dc094a4
Irfankey <mirfanmaulana0315@gmail.com>
2022-04-25 19:49:56 +0700

    first commit

---
+++ pustaka-booking/application/controllers/contoh_1.php
@@ -0,0 +1,15 @@
+<?php
+
+class contoh_1 extends CI_Controller {
+public function index()
+	{
+		echo "Biodata Saya<br>";
+		echo "Nama Lengkap : Irfan Maulana<br>";
+		echo "Nim : 17190089<br>";
+		echo "Fakultas : Teknik dan Informatika<br>";
+		echo "Jurusan : Teknologi Informasi<br>";
+		echo "Jenis Kelamin : Laki-Laki<br>";
+		echo "Usia : 21 Tahun<br>";
+		echo "Hobbi : Bermain Games<br>";
+	}
+}
